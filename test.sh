3b49c35b5bfa57a0c856ed4f0aca8afaa8ca228b
Alina Harutyunyan <alinaharutyunyants@gmail.com>
2019-09-20 23:27:57 +0400

    iniital commit

--- test.sh
+++ test.sh
@@ -1,2 +1,1 @@
-#replace awk or fmt part with 'lowercase' or 'makewords' program of your own to test
-cat text_to_spell | awk '{print tolower($0)}' | fmt -1 | uniq | sort| sed 's/\.//g' | comm -13 dictionary -
+cat /home/alina/Desktop/OS/assignment00-AlinaHar/text_to_spell.txt | ./lowercase | fmt -1 |uniq | sort |sed 's/\.//g' | comm -13 /home/alina/Desktop/OS/assignment00-AlinaHar/dictionary.txt -
